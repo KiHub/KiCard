c87e178292ad5fc16be1a18e90cd78768c195373
Kirill Ki <kirillkalapov@gmail.com>
2021-12-27 13:53:54 +0100

    Initial Commit

---
+++ KiCard/KiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiCardApp.swift
+//  KiCard
+//
+//  Created by  Mr.Ki on 27.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct KiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
